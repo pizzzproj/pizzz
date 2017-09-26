c0eeae21935aa56abac8d13b645b4240b595073e
Matt Rathburn <m.rathburn@outlook.com>
2017-09-26 11:41:02 -0400

    AboutController Commit

---
+++ pizzzproj/pizzzproj/Controllers/AboutController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using pizzzproj.Models;
+
+namespace pizzzproj.Controllers
+{
+    public class AboutController : Controller
+    {
+
+        public IActionResult Index()
+        {
+           return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "This is a test";
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "ContactPage";
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
