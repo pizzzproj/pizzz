81449d9540bcf6526c0a0dcbd1f7464eaf9b0ed1
Matt Rathburn <m.rathburn@outlook.com>
2017-09-26 20:52:05 -0400

    AddPizzaCommit

---
+++ pizzzproj/pizzzproj.Logic/OrderPizzaLogic.cs
@@ -0,0 +1,26 @@
+﻿using pizzzproj.Data.Helper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pizzzproj.Logic
+{
+    class OrderPizzaLogic
+    {
+        public int PizzaId { get; set; }
+        public string PizzaName { get; set; }
+        public string PizzaSizeId { get; set; }
+        
+        public static OrderPizzaLogic AddPizza(Pizza pizza)
+        {
+            var addPizza = new OrderPizzaLogic()
+            {
+                PizzaId = pizza.PizzaId,
+                PizzaName = pizza.PizzaName,
+                PizzaSizeId = pizza.PizzaSizeId
+            };
+
+            return addPizza;
+        }
+    }
+}
