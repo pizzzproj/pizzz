70df95ee37926d6fbf96299ef6157a35cc42a19b
Jacob Burton <jacobaseball44@yahoo.com>
2017-09-26 19:05:21 -0700

    added add pizza item to test project

--- pizzzproj/pizzzproj.Test/UnitTest1.cs
+++ pizzzproj/pizzzproj.Test/UnitTest1.cs
@@ -1,14 +1,22 @@
 using System;
 using Xunit;
+using pizzzproj.Logic;
+using pizzzproj.Data;
+using pizzzproj.Test;
 
 namespace pizzzproj.Test
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void addOrderItem()
         {
+            var i = new OrderPizzaLogic() { PizzaID = 1, PizzaName = "Cheese Lovers", PizzaSize = "Small" };
+          //  var j = new OrderPizzaLogic() { IDisposable = 2, Name = "Pepperoni", Size = "Large" };
 
+            var actual = i.AddPizza();
+
+            Assert.True(actual);
         }
     }
 }
