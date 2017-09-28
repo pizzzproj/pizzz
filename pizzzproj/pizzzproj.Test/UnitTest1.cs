44f8d899c909a5a9368c9c6381c2caabcd606f01
Matt Rathburn <m.rathburn@outlook.com>
2017-09-27 20:21:38 -0400

    Added Stuffs

--- pizzzproj/pizzzproj.Test/UnitTest1.cs
+++ pizzzproj/pizzzproj.Test/UnitTest1.cs
@@ -10,18 +10,26 @@ namespace pizzzproj.Test
         [Fact]
         public void Test1()
         {
+            
             var i = new Pizza() { PizzaId = 1, PizzaName = "Cheese", PizzaSizeId = "small" };
 
-            var actual = AddPizza(i);
+            var actual = OrderPizzaLogic.AddPizza(i);
 
-            Assert.True(actual);
-
+            Assert.Equal(actual.PizzaId, i.PizzaId);
+            Assert.Equal(actual.PizzaName, i.PizzaName);
+            Assert.Equal(actual.PizzaSizeId, i.PizzaSizeId);
         }
 
         [Fact]
         public void Test2()
         {
-            //var i = new OrderPizzaLogic() { PizzaId = 1; PizzaName = "Cheese", PizzaSizeId = "Small" };
+            var i = new Side() { SideId = 1, SideName = "Wings", SideSize = "Family" };
+
+            var actual = OrderSideLogic.AddSide(i);
+
+            Assert.Equal(actual.SideId, i.SideId);
+            Assert.Equal(actual.SideName, i.SideName);
+            Assert.Equal(actual.SideSize, i.SideSize);
         }
     }
 }
