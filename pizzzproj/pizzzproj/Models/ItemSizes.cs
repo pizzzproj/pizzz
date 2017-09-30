7d749954d1fbeb339e168b314225896cb328cdce
elijah <elijahe.joseph@outlook.com>
2017-09-29 18:38:27 -0700

    test

---
+++ pizzzproj/pizzzproj/Models/ItemSizes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace pizzzproj.Models
+{
+    public partial class ItemSizes
+    {
+        public int SizeId { get; set; }
+        public string Size { get; set; }
+    }
+}
