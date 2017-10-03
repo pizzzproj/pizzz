f815bd8308f874d6b632a27d3888d736646a865a
Jacob Burton <jacobaseball44@yahoo.com>
2017-10-02 17:59:27 -0700

    added controller method

--- pizzzproj/pizzzproj.LogicDTO/Controllers/ItemController.cs
+++ pizzzproj/pizzzproj.LogicDTO/Controllers/ItemController.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using pizzzproj.LogicDTO.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace pizzzproj.LogicDTO.Controllers
 {
@@ -34,12 +37,45 @@ namespace pizzzproj.LogicDTO.Controllers
             return "value";
         }
         
-        // POST: api/Item
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
         
+        // POST: api/Item
+        /*
+        [HttpPost]
+        public void PizzaPost([FromBody]int id)
+        {
+            HttpClient pizzclient = new HttpClient();
+            var yo = JsonConvert.SerializeObject(id);
+            var body = new StringContent(yo, Encoding.UTF32, "application/json");
+            var res = pizzclient.GetAsync("http://localhost:58080/api/menuitemprice/", body).GetAwaiter().GetResult();
+
+            if(res.IsSuccessStatusCode)
+            {
+                Response.StatusCode = (int)HttpStatusCode.Created;
+            }
+            else
+            {
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            }
+        }
+        */
+
+        [HttpGet]
+        public decimal PizzaGetPrice(int i)
+        {
+            HttpClient pizzclient = new HttpClient();
+
+            var res = pizzclient.GetAsync("http://localhost:58080/api/menuitemprice/" + i).GetAwaiter().GetResult();
+
+            if(res.IsSuccessStatusCode)
+            {
+                var json = res.Content.ReadAsStringAsync().Result;
+                var driver = JsonConvert.DeserializeObject<decimal>(json);
+                Response.StatusCode = (int)HttpStatusCode.OK;
+                return driver;
+            }
+            return 0;
+        }
+
         // PUT: api/Item/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
