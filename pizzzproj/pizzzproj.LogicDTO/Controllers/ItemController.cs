d58f45161162ebe2ac77f4aa27fa0436835232c5
Matt Rathburn <m.rathburn@outlook.com>
2017-10-03 21:12:59 -0400

    Controller Changes

--- pizzzproj/pizzzproj.LogicDTO/Controllers/ItemController.cs
+++ pizzzproj/pizzzproj.LogicDTO/Controllers/ItemController.cs
@@ -17,14 +17,14 @@ namespace pizzzproj.LogicDTO.Controllers
     public class ItemController : Controller
     {
 
-        
+        /*
         [HttpPost]
         public void NewItemPost([FromBody]Item item)
         {
             HttpClient pizzclient = new HttpClient();
             var yo = JsonConvert.SerializeObject(item);
-            var body = new StringContent(yo, Encoding.UTF32, "application/json");
-            var res = pizzclient.GetAsync("http://localhost:58080/api/getmenu/", body).GetAwaiter().GetResult();
+            var body = new StringContent(yo, Encoding.UTF8, "application/json");
+            var res = pizzclient.PostAsync("http://localhost:58080/api/getmenu/", body).GetAwaiter().GetResult();
 
             if(res.IsSuccessStatusCode)
             {
@@ -34,7 +34,7 @@ namespace pizzzproj.LogicDTO.Controllers
             {
                 Response.StatusCode = (int)HttpStatusCode.BadRequest;
             }
-        }
+        }*/
         
         
 
@@ -63,6 +63,22 @@ namespace pizzzproj.LogicDTO.Controllers
             var res = orderclient.GetAsync("http://localhost:58080/api/getmenu/").GetAwaiter().GetResult();
 
             if (res.IsSuccessStatusCode)
+            {
+                var json = res.Content.ReadAsStringAsync().Result;
+                var driver = JsonConvert.DeserializeObject<Item>(json);
+                Response.StatusCode = (int)HttpStatusCode.OK;
+                return driver;
+            }
+            return null;
+        }
+
+        [HttpGet]
+        public Item GetItem(int id)
+        {
+            HttpClient httpClient = new HttpClient();
+            var res = httpClient.GetAsync("http://localhost:58080/api/menuitemprice/" + id).GetAwaiter().GetResult();
+
+            if(res.IsSuccessStatusCode)
             {
                 var json = res.Content.ReadAsStringAsync().Result;
                 var driver = JsonConvert.DeserializeObject<Item>(json);
