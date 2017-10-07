9f91e074fd9f1fc93e8eecf5be747101b715995f
Achintya Goel <goel.achintya@gmail.com>
2017-10-07 17:21:18 -0400

    Reformatted Menu

---
+++ pizzzadata/pizzzadata.API/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pizzzadata.API.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Size { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
