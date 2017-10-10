54fdd10b2ecf41a6c94b2b0639f63bc0534e483a
Matt Rathburn <m.rathburn@outlook.com>
2017-10-10 14:21:30 -0400

    A ton of changes

---
+++ pizzzUI/FakeUI/Controllers/ItemsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FakeUI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Items")]
+    public class ItemsController : Controller
+    {
+        // GET: api/Items
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Items/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/Items
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/Items/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ pizzzUI/pizzzUIAng/Models/Items.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pizzzUIAng.Models
+{
+    public class Items
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public string ItemSize { get; set; }
+        public decimal ItemPrice { get; set; }
+    }
+}
