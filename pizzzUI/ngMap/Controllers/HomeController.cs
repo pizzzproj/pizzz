2b5f0d58aa45431fdbd54e1705383210afc1435b
elijah <elijahe.joseph@outlook.com>
2017-10-10 13:20:31 -0700

    Added Angular

---
+++ pizzzUI/ngMap/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Reflection;
+
+namespace ngMap.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetAllLocation()
+        {
+            using (pizzzaDatabaseEntities2 pde = new pizzzaDatabaseEntities2())
+            {
+                var v = pde.Locations.OrderBy(a => a.Title).ToList();
+                return new JsonResult { Data = v, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+
+        public JsonResult GetMarkerInfo(int locationID)
+        {
+            using (pizzzaDatabaseEntities2 pde = new pizzzaDatabaseEntities2())
+            {
+                Location l = null;
+                l = pde.Locations.Where(a => a.LocationID.Equals(locationID)).FirstOrDefault();
+                return new JsonResult { Data = l, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+
+    }
+}
--- pizzzUI/pizzzUIMap/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="pizzzUIMap.MvcApplication" Language="C#" %>
